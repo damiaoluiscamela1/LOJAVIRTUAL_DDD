5142d80b396a8ab439f767567122048f6bc4221f
Damião Luis <daiman816@gmail.com>
2021-07-08 11:20:46 +0100

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,47 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<CompraUsuario> CompraUsuario { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Server=(localdb)\\mssqllocaldb;Database=aspnet-Ecomerce_WEB-705D1050-0C23-470B-9B1F-43765565FA3F;Trusted_Connection=True;MultipleActiveResultSets=true";
+            return strCon;
+        }
+
+
+    }
+}
---
+++ Web_ECommerce/Controllers/ProdutosController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationApp.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_ECommerce.Controllers
+{
+
+    [Authorize]
+
+    public class ProdutosController : Controller
+    {
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public readonly InterfaceProductApp _InterfaceProductApp;
+
+        public ProdutosController(InterfaceProductApp InterfaceProductApp, UserManager<ApplicationUser> userManager)
+        {
+            _InterfaceProductApp = InterfaceProductApp;
+            _userManager = userManager;
+        }
+
+        // GET: ProdutosController
+        public async Task<IActionResult> Index()
+        {
+            var idUsuario = await RetornarIdUsuarioLogado();
+
+            return View(await _InterfaceProductApp.ListarProdutosUsuario(idUsuario));
+        }
+
+        // GET: ProdutosController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _InterfaceProductApp.GetEntityById(id));
+        }
+
+        // GET: ProdutosController/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Produto produto)
+        {
+            try
+            {
+
+                var idUsuario = await RetornarIdUsuarioLogado();
+
+                produto.UserId = idUsuario;
+
+                await _InterfaceProductApp.AddProduct(produto);
+                if (produto.Notitycoes.Any())
+                {
+                    foreach (var item in produto.Notitycoes)
+                    {
+                        ModelState.AddModelError(item.NomePropriedade, item.mensagem);
+                    }
+
+                    return View("Create", produto);
+                }
+
+            }
+            catch
+            {
+                return View("Create", produto);
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // GET: ProdutosController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await _InterfaceProductApp.GetEntityById(id));
+        }
+
+        // POST: ProdutosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Produto produto)
+        {
+            try
+            {
+                await _InterfaceProductApp.UpdateProduct(produto);
+                if (produto.Notitycoes.Any())
+                {
+                    foreach (var item in produto.Notitycoes)
+                    {
+                        ModelState.AddModelError(item.NomePropriedade, item.mensagem);
+                    }
+
+                    return View("Edit", produto);
+                }
+
+            }
+            catch
+            {
+                return View("Edit", produto);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ProdutosController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await _InterfaceProductApp.GetEntityById(id));
+        }
+
+        // POST: ProdutosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Produto produto)
+        {
+            try
+            {
+                var produtoDeletar = await _InterfaceProductApp.GetEntityById(id);
+
+                await _InterfaceProductApp.Delete(produtoDeletar);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private async Task<string> RetornarIdUsuarioLogado()
+        {
+            var idUsuario = await _userManager.GetUserAsync(User);
+
+            return idUsuario.Id;
+        }
+
+    }
+}
